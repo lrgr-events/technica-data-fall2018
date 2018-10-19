cfbdbadf7c63515d025706dd7f677800120285ca
Xuan Li <xuanli94@cbcbgpu02.umiacs.umd.edu>
2018-10-19 14:42:35 -0400

    a bash script for downloading CCLE data needed for drug response

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O CCLE_miRNA_expression_data.gct https://data.broadinstitute.org/ccle/CCLE_miRNA_20180525.gct
+wget -O CCLE_RNAseq_RPKM_data.gct https://data.broadinstitute.org/ccle/CCLE_DepMap_18q3_RNAseq_RPKM_20180718.gct
+wget -O CCLE_DNA_methylation_CpG_data.tsv https://data.broadinstitute.org/ccle/CCLE_RRBS_TSS_CpG_clusters_20180614.txt
