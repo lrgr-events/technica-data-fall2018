0048283b04a4f8897553c4c6af041b562e2a8d94
Jason Fan <jason.fan.74@gmail.com>
2018-11-08 18:03:54 -0500

    ccle to broad name conversion

--- scripts/CCLE.py
+++ scripts/CCLE.py
@@ -5,21 +5,32 @@ class CCLE_Info(object):
     def __init__(self, ccle_meta_file):
         self.fp = ccle_meta_file
         self.df = pd.read_csv(ccle_meta_file, header=0)
+        self.broad_ids = list(self.df['DepMap_ID'])
+        self.ccle_names = list(self.df['CCLE_Name'])
+        self._broad2ccle = dict(zip(self.broad_ids, self.ccle_names))
+        self._ccle2broad = dict(zip(self.ccle_names, self.broad_ids))
 
-    def broad_id_2_ccle_name(broad_id):
-        pass
-    def ccle_name_2_broad_id(ccle_name):
-        pass
+    def broad_id_2_ccle_name(self, broad_id):
+        '''
+        Convert Broad ID to CCLE name
+        '''
+        return self._broad2ccle[broad_id]
+
+    def ccle_name_2_broad_id(self, ccle_name):
+        '''
+        Convert CCLE name to  Broad ID
+        '''
+        return self._ccle2broad[ccle_name]
     
-    def get_ccle_name_info(ccle_name):
+    def get_ccle_name_info(self, ccle_name):
         '''
         Return dictionary of info given ccle_name
         '''
-        raise NotImplemented
+        raise NotImplementedError
     
-    def get_broad_id_name_info(broad_id):
+    def get_broad_id_name_info(self, broad_id):
         '''
         Return dictionary of info given ccle_name
         '''
-        raise NotImplemented
+        raise NotImplementedError
         
--- scripts/tests.py
+++ scripts/tests.py
@@ -8,9 +8,12 @@ class TestALL(unittest.TestCase):
     def setUp(self):
         pass
 
-    def CCLE_info(self):
-        self.ccle_info = CCLE_Info('../data/raw/CCLE_metadata.csv')
-        self.assertTrue(True)
+    def test_CCLE_info(self):
+        ccle_info = CCLE_Info('../data/raw/CCLE_metadata.csv')
+        self.assertEqual(ccle_info.broad_id_2_ccle_name('ACH-000001'), 'NIHOVCAR3_OVARY')
+        self.assertEqual(ccle_info.ccle_name_2_broad_id('NIHOVCAR3_OVARY'),'ACH-000001' )
+
+    
 
 if __name__ == '__main__':
     unittest.main()
